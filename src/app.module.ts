d2ba50789e5530674e13ea8804404847b80c3330
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2025-04-23 20:33:45 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { WalletModule } from './wallet/wallet.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [PrismaModule, AuthModule, WalletModule, TransactionsModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
