666c7fe10927e8ef3f2b5f6d958967b12c07b95b
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2025-04-25 11:44:55 -0300

    Commit msg

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { WalletModule } from './wallet/wallet.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { UsersModule } from './users/users.module';
-import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule, AuthModule, WalletModule, TransactionsModule, UsersModule],
