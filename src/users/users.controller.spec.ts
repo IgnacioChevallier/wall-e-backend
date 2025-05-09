c1b96e833525b8342c59203ed2ea6d29e46c0cc6
matias <mchialva@mail.austral.edu.ar>
2025-05-09 16:37:32 -0300

    Test suit passes. wallet functionality endpoints.

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,17 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserRepository } from './user.repository';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
+
+  const mockPrismaService = {
+    user: {
+      findMany: jest.fn(),
+      findUnique: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  };
+
+  const mockUserRepository = {
+    findUserByEmail: jest.fn(),
+    createUser: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+        {
+          provide: UserRepository,
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
